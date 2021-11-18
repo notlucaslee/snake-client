851c22ccb0b736571b68895d73ebfefe05b5465b
notlucaslee <lee.lucas.hyunguk@gmail.com>
2021-11-18 06:32:14 +0000

    added controls

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = conn => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,9 +11,23 @@ const setupInput = function() {
 };
 
 const handleUserInput = key => {
-  if (key === '\u0003') {
+  switch (key) {
+  case '\u0077':
+    connection.write("Move: up");
+    break;
+  case '\u0061':
+    connection.write("Move: left");
+    break;
+  case '\u0073':
+    connection.write("Move: down");
+    break;
+  case '\u0064':
+    connection.write("Move: right");
+    break;
+  case '\u0003':
     process.exit();
   }
+  
 };
 
 module.exports = {setupInput};
--- play.js
+++ play.js
@@ -2,5 +2,5 @@ const {connect} = require('./client');
 const {setupInput} = require('./input');
 // establishes a connection with the game server
 console.log('Connecting...');
-connect();
-setupInput();
+
+setupInput(connect());
