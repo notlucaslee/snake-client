046d8f225fb3d804ce41b82d65b07637be8e890d
notlucaslee <lee.lucas.hyunguk@gmail.com>
2021-11-18 05:57:11 +0000

    added userInput starts

--- play.js
+++ play.js
@@ -1,4 +1,18 @@
 const {connect} = require('./client');
 // establishes a connection with the game server
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = key => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+setupInput();
 connect();
